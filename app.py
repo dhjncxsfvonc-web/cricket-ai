6164890eed506d0e0c2d0d1b79eb72d61720158f
Mayank Singh <dhjncxsfvonc@gmail.com>
2026-05-04 03:34:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "App chal raha hai 🔥"
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
